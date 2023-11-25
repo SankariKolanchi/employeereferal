21e6f5cd54aa6cc5c221ce1b86d7cb811cef23ec
Sankaripriya <sankari@gmail.com>
2023-11-25 14:00:24 +0530

    splash bug fix and signup

--- lib/view/screens/signup_screen.dart
+++ lib/view/screens/signup_screen.dart
@@ -4,112 +4,184 @@ class SignUpScreen extends StatelessWidget {
   const SignUpScreen({
     Key? key,
   }) : super(key: key);
-  
 
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    print('height ==> $height, height / 10 ==> ${height / 10}');
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              color: Colors.blue,
-              image: DecorationImage(
-                image: AssetImage(
-                  'assets/images/bg_img.png',
-                ),
-                fit: BoxFit.cover,
-              ),
-            ),
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Image.asset(
-                    'assets/images/logo.png',
-                    height: 100, // Adjust the height as needed
+      backgroundColor: Colors.blue, 
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: height,
+          child: Stack(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.blue,
+                      image: DecorationImage(
+                        image: AssetImage(
+                          'assets/images/bg_img.png',
+                        ),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: Center(
+                  Positioned(
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
                     child: Container(
+                      color: Colors.transparent,
+                      child: Image.asset(
+                        'assets/images/emp_queue.png',
+                        width: double.infinity,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 40, right: 40, top: height / 10),
+                child: Column(
+                  children: [
+                    Container(
                       padding: const EdgeInsets.all(15),
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(12),
-                        boxShadow: [
+                        boxShadow: const [
                           BoxShadow(
-                            color: Colors.grey.shade300,
+                            color: Colors.grey,
                             spreadRadius: 2,
-                            blurRadius: 0.2,
+                            blurRadius: 5,
                           ),
                         ],
                         color: Colors.white,
                       ),
-                      child: SingleChildScrollView(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            TextFormField(
-                              decoration: const InputDecoration(
-                                labelText: 'Name',
-                                border: OutlineInputBorder(),
-                              ),
-                            ),
-                            const SizedBox(height: 16),
-                            TextFormField(
-                              decoration: const InputDecoration(
-                                labelText: 'Employee ID',
-                                border: OutlineInputBorder(),
-                              ),
-                            ),
-                            const SizedBox(height: 16),
-                            TextFormField(
-                              decoration: const InputDecoration(
-                                labelText: 'Phone Number',
-                                border: OutlineInputBorder(),
-                              ),
-                            ),
-                            const SizedBox(height: 16),
-                            ElevatedButton(
-                              onPressed: () {},
-                              child: const Text('Sign Up'),
-                            ),
-                            const SizedBox(height: 16),
-                            const Divider(
-                              thickness: 0.5,
-                              endIndent: double.infinity,
-                            ),
-                            const Text("Or"),
-                            const Divider(
-                              thickness: 2,
-                              endIndent: double.infinity,
-                            ),
-                            ElevatedButton(
-                              onPressed: () {},
-                              child: const Text('login'),
-                            ),
-                          ],
-                        ),
+                      child: Image.asset(
+                        'assets/images/logo.png',
+                        height: 50,
                       ),
                     ),
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 25),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          TextFormField(
+                            style: const TextStyle(color: Colors.white),
+                            decoration: const InputDecoration(
+                              labelText: 'Name',
+                              labelStyle: TextStyle(color: Colors.white),
+                              border: OutlineInputBorder(),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          TextFormField(
+                            style: const TextStyle(color: Colors.white),
+                            decoration: const InputDecoration(
+                              labelText: 'Employee ID',
+                              labelStyle: TextStyle(color: Colors.white),
+                              border: OutlineInputBorder(),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          TextFormField(
+                            style: const TextStyle(color: Colors.white),
+                            decoration: const InputDecoration(
+                              labelText: 'Phone Number',
+                              labelStyle: TextStyle(color: Colors.white),
+                              border: OutlineInputBorder(),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                            ),
+                            child: const Padding(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 40,
+                                vertical: 12,
+                              ),
+                              child: Text(
+                                'Sign Up',
+                                style: TextStyle(color: Colors.blue),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          const Row(
+                            children: [
+                              Expanded(
+                                child: Divider(
+                                  thickness: 2,
+                                  endIndent: 3,
+                                ),
+                              ),
+                              Text("Or", style: TextStyle(color: Colors.white)),
+                              Expanded(
+                                child: Divider(
+                                  thickness: 2,
+                                  indent: 3,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 16),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              primary: Colors.white,
+                              elevation: 0,
+                            ),
+                            child: const Padding(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 40,
+                                vertical: 12,
+                              ),
+                              child: Text(
+                                'Login',
+                                style: TextStyle(color: Colors.blue),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              color: Colors.transparent,
-              child: Image.asset(
-                'assets/images/emp_queue.png',
-                width: double.infinity,
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
